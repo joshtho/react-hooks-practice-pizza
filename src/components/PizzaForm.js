61603dd32a6f6b8fc7e167fb257bc10e96c53f5b
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-09-04 18:06:53 -0500

    commit tree 5ceb5866916f22a7391e19a744fab91ff2a45071

--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,6 +1,8 @@
 import React from "react";
 
-function PizzaForm() {
+function PizzaForm({editPizza, handleChange}) {
+
+  
   return (
     <form onSubmit={null /*handle that submit*/}>
       <div className="form-row">
@@ -10,13 +12,16 @@ function PizzaForm() {
             type="text"
             name="topping"
             placeholder="Pizza Topping"
+            value={editPizza.topping}
           />
         </div>
         <div className="col">
-          <select className="form-control" name="size">
+          <select className="form-control" name="size" value={editPizza.size}  >
+
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
+          
           </select>
         </div>
         <div className="col">
@@ -26,7 +31,8 @@ function PizzaForm() {
               type="radio"
               name="vegetarian"
               value="Vegetarian"
-            />
+              checked={editPizza.vegetarian ? true : false}
+              />
             <label className="form-check-label">Vegetarian</label>
           </div>
           <div className="form-check">
@@ -35,6 +41,7 @@ function PizzaForm() {
               type="radio"
               name="vegetarian"
               value="Not Vegetarian"
+              checked={editPizza.vegetarian ? false : true}
             />
             <label className="form-check-label">Not Vegetarian</label>
           </div>
