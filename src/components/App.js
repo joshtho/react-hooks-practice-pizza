8dcc7cb9ca3f4848addb444dc2749af3ec997167
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-09-04 18:07:11 -0500

    commited

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Header from "./Header";
 import PizzaForm from "./PizzaForm";
 import PizzaList from "./PizzaList";
 
 function App() {
+  const [pizzas, setPizzas] = useState([])
+  const [editPizza, setEditPizza] = useState({
+    topping: '',
+    size: '',
+    vegetarian: true
+  })
+  
+  useEffect(() => {
+    fetch("http://localhost:3001/pizzas")
+    .then(r => r.json())
+    .then(data => setPizzas(data))
+  }, [])
+  
+  
   return (
     <>
       <Header />
-      <PizzaForm />
-      <PizzaList />
+      <PizzaForm editPizza={editPizza} />
+      <PizzaList pizzas={pizzas} setEditPizza={setEditPizza} />
     </>
   );
 }
--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,13 +1,21 @@
 import React from "react";
 
-function Pizza() {
+function Pizza({pizza, setEditPizza}) {
   return (
     <tr>
-      <td>{"Replace Me With Pizza Topping"}</td>
-      <td>{"Replace Me With Pizza Size"}</td>
-      <td>{"Replace Me With Vegatarian"}</td>
+      <td>{pizza.topping}</td>
+      <td>{pizza.size}</td>
+      <td>{pizza.vegetarian ? "Yes" : "No" }</td>
       <td>
-        <button type="button" className="btn btn-primary">
+        <button 
+        type="button" 
+        className="btn btn-primary" 
+        onClick={() => setEditPizza({...pizza, 
+          topping: pizza.topping,
+          size: pizza.size,
+          vegetarian: pizza.vegetarian,
+        })}
+        >
           Edit Pizza
         </button>
       </td>
